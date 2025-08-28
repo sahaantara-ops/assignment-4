424f1598f2bb8e4dfa50bdc8f14247ae8203dfd6
sahaantara567 <sahaantara567@gmail.com>
2025-08-29 01:20:53 +0600

    add banner items

--- script/script.js
+++ script/script.js
@@ -1,1 +1,18 @@
 console.log(document);
+// Step 1: Select all heart buttons
+const icon2 = document.querySelectorAll('.icon-2');
+
+// Step 2: Select the Navbar counter
+
+
+const count = document.getElementById('navbar-heart-count');
+
+
+// Step 3: Add click event to each heart button
+icon2.forEach(icon2 => {
+  icon2.addEventListener('click', () => {
+    // Convert current count to number and increment
+    let currentCount = parseInt(count.innerText);
+    count.innerText = currentCount + 1;
+  });
+});
